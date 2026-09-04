5f9f1b70c7a366e7ded753bf1af4645b1a82bb64
unknown <prashanandradi@gmail.com>
2026-09-04 17:23:34 +0530

    Require auth on T-Bill status updates.

--- routes/tbillRoutes.js
+++ routes/tbillRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const tbillController = require('../controllers/tbillController');
+const { checkAuth } = require('../middleware/auth');
 
 router.get('/recent', tbillController.getRecent);
 router.get('/buy-deals', tbillController.getBuyDealsWithBalance);
 router.post('/', tbillController.create);
-router.put('/:id/status', tbillController.updateStatus);
+router.put('/:id/status', checkAuth, tbillController.updateStatus);
 router.put('/:id', tbillController.update);
 
 module.exports = router;
